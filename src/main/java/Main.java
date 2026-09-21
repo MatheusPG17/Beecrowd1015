e72f59f2adb67da4b0fdbb4e42b937772c79b40a
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 16:18:21 -0300

    add: comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         String linha, linha2;
         String[] lista, lista2;
         double x1, x2, y1, y2, dist;
         
+        //entrada de dados
         linha = leia.nextLine();
         linha2 = leia.nextLine();
         
+        //conversão de linha para lista
         lista = linha.split(" ");
         x1 = Double.parseDouble(lista[0]);
         y1 = Double.parseDouble(lista[1]);
@@ -18,8 +21,10 @@ public class Main {
         x2 = Double.parseDouble(lista2[0]);
         y2 = Double.parseDouble(lista2[1]);
         
+        //processamento
         dist = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1), 2));
         
+        //saida de dados 
         System.out.printf("%.4f\n", dist);
     }
 }
