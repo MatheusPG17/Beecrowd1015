87bd8d74bec7d2f36eb1c8f0648c54c8ac82b072
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 16:15:35 -0300

    add: Resolução Beecrowd1015

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        String linha, linha2;
+        String[] lista, lista2;
+        double x1, x2, y1, y2, dist;
+        
+        linha = leia.nextLine();
+        linha2 = leia.nextLine();
+        
+        lista = linha.split(" ");
+        x1 = Double.parseDouble(lista[0]);
+        y1 = Double.parseDouble(lista[1]);
+        lista2 = linha2.split(" ");
+        x2 = Double.parseDouble(lista2[0]);
+        y2 = Double.parseDouble(lista2[1]);
+        
+        dist = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1), 2));
+        
+        System.out.printf("%.4f\n", dist);
+    }
+}
